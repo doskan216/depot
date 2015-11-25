6b2a6b40600b32a456297e92852215906ebde409
doskan216 <you@example.com>
2015-11-25 00:38:04 +0000

    国際化終わり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :now
   before_action :authorize
+  before_action :set_i18n_locale_form_params
 
   private
 
@@ -22,6 +23,22 @@ class ApplicationController < ActionController::Base
   end
 
   def now
-    @time = Time.now
+    @time = Time.zone.now
+  end
+
+  def set_i18n_locale_form_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] =
+        "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,13 @@ class StoreController < ApplicationController
   def index
     @products = Product.all.order(:title)
 
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @cart = current_cart
+    end
+
     if session[:counter].nil?
       session[:counter] = 1
     else
